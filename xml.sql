73ad3f70e40a2aec1ae7147ad8da88b7ae640ee8
Yusaira Khan <yusaira.khan@mail.mcgill.ca>
2016-03-19 20:02:01 -0400

    1 query working

--- xml.sql
+++ xml.sql
@@ -5,32 +5,34 @@ CREATE TABLE Take_Ride (
         FOREIGN KEY (rid) REFERENCES Ride,
         FOREIGN KEY (uid) REFERENCES User,
         PRIMARY KEY (rid, uid),
-        RideRequestInfo XML Not Null
+        INFO XML Not Null
 );
 
 
-INSERT INTO Take_Ride (rid, uid,RideRequestInfo)
+INSERT INTO Take_Ride (rid, uid,INFO)
 VALUES(1, 2, '<session><source users="1" space_left="1">initiator</source><time_accepted>10:00</time_accepted><date>12-12-2012</date><uid>2</uid></session>');
 
-INSERT INTO Take_Ride (rid, uid,RideRequestInfo)
-VALUES(2, 1, '<session><source users="1" space_left="3">initiator</source> <time_accepted>11:00</time_accepted><date>12-12-2012</date><uid>2</uid></session>');
+INSERT INTO Take_Ride (rid, uid,INFO)
+VALUES(2, 1, '<session><source users="1" space_left="3">initiator</source> <time_accepted>11:00</time_accepted><date>12-12-2012</date><uid>1</uid></session>');
 
-INSERT INTO Take_Ride (rid, uid,RideRequestInfo)
-VALUES(2, 3, '<session><source users="2" space_left="2">splitter</source> <time_accepted>11:05</time_accepted><date>12-12-2012</date><uid>2</uid></session>');
+INSERT INTO Take_Ride (rid, uid,INFO)
+VALUES(2, 3, '<session><source users="2" space_left="2">splitter</source> <time_accepted>11:05</time_accepted><date>12-12-2012</date><uid>3</uid></session>');
 
-INSERT INTO Take_Ride (rid, uid,RideRequestInfo)
-VALUES(3, 5, '<session><source users="1" space_left="3">initiator</source> <time_accepted>12:00</time_accepted><date>12-12-2012</date><uid>2</uid></session>');
+INSERT INTO Take_Ride (rid, uid,INFO)
+VALUES(3, 5, '<session><source users="1" space_left="3">initiator</source> <time_accepted>12:00</time_accepted><date>12-12-2012</date><uid>5</uid></session>');
 
-INSERT INTO Take_Ride (rid, uid,RideRequestInfo)
-VALUES(4, 4, '<session><source users="1" space_left="1">initiator</source> <time_accepted>12:00</time_accepted><date>12-12-2012</date><uid>2</uid></session>');
+INSERT INTO Take_Ride (rid, uid,INFO)
+VALUES(4, 4, '<session><source users="1" space_left="1">initiator</source> <time_accepted>12:00</time_accepted><date>12-12-2012</date><uid>4</uid></session>');
 
-INSERT INTO Take_Ride (rid, uid,RideRequestInfo)
+INSERT INTO Take_Ride (rid, uid,INFO)
 VALUES(4, 2, '<session><source users="2" space_left="0">splitter</source> <time_accepted>12:05</time_accepted><date>12-12-2012</date><uid>2</uid></session>');
 
-INSERT INTO Take_Ride (rid, uid,RideRequestInfo)
-VALUES(5, 3, '<session><source users="1" space_left="1">initiator</source> <time_accepted>13:00</time_accepted><date>12-12-2012</date><uid>2</uid></session>');
+INSERT INTO Take_Ride (rid, uid,INFO)
+VALUES(5, 3, '<session><source users="1" space_left="1">initiator</source> <time_accepted>13:00</time_accepted><date>12-12-2012</date><uid>3</uid></session>');
 
 select * from Take_Ride;
 
-SELECT XMLQUERY ('for $d in $doc/session return <splitter_uid>$d/uid</splitter_uid>' passing RideRequestInfo  'doc')
-FROM Take_Ride m WHERE XMLEXISTS ('$doc/session[source="splitter"]' passing m.RideRequestInfo as 'doc');
+SELECT XMLQUERY ( 'for $d in $INFO/session 
+			where $d/source="splitter"
+			return <splitter> {$d/uid} </splitter> ' )
+FROM Take_Ride WHERE XMLEXISTS ('$INFO/session[source="splitter"]' );
